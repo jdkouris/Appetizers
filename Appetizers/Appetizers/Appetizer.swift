f143c8cca3e272cd0ed76e19648e747921626acf
John Kouris <jdkouris@gmail.com>
2021-03-03 14:09:38 -0800

    Added mock data image and list of appetizers for mocking

--- Appetizers/Appetizers/Appetizer.swift
+++ Appetizers/Appetizers/Appetizer.swift
@@ -24,4 +24,5 @@ struct AppetizerResponse: Decodable {
 
 struct MockData {
     static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99)
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
 }
