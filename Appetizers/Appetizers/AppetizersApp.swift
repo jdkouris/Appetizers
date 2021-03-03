f74f6216ab519836bc1265acbd4f68aaba863e5a
John Kouris <jdkouris@gmail.com>
2021-03-03 13:31:50 -0800

    Created starter app

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
