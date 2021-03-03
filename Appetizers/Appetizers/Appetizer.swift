3312a590a2e055968af5f19d621fd0d24a6c7587
John Kouris <jdkouris@gmail.com>
2021-03-03 14:03:42 -0800

    Created appetizer model

---
+++ Appetizers/Appetizers/Appetizer.swift
@@ -0,0 +1,19 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/3/21.
+//
+
+import Foundation
+
+struct Appetizer {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
