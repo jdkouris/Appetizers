5ab980fdcf1536cd42b339ef8e15b4c687d4e0c5
John Kouris <jdkouris@gmail.com>
2021-03-03 14:47:49 -0800

    Implemented the getAppetizers method in the NetworkManager

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/3/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
