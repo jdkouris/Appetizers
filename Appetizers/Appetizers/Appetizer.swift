2c94008d88a6ca184d93776dc8abd431ff795a80
John Kouris <jdkouris@gmail.com>
2021-03-03 14:26:45 -0800

    Created the appetizer list cells and populated the list with mock data

--- Appetizers/Appetizers/Appetizer.swift
+++ Appetizers/Appetizers/Appetizer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetizer: Decodable {
+struct Appetizer: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
@@ -23,6 +23,6 @@ struct AppetizerResponse: Decodable {
 }
 
 struct MockData {
-    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99)
+    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageURL: "asian-flank-steak", calories: 99, protein: 99, carbs: 99)
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
 }
