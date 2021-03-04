e466b51ad49087834eb6a7ad9f16db696bbe86ed
John Kouris <jdkouris@gmail.com>
2021-03-03 16:04:14 -0800

    Added Color extension file and LoadingView file

---
+++ Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/3/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
---
+++ Appetizers/Appetizers/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/3/21.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = UIColor(named: "brandPrimary")
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+    
+}
