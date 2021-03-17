80682e9550b06616fc766d58fe8a40b6e0702d1f
John Kouris <jkouris@nexient.com>
2021-03-17 08:44:47 -0700

    Implemented User and refactored

--- Appetizers/Appetizers/Model/User.swift
+++ Appetizers/Appetizers/Model/User.swift
@@ -9,6 +9,11 @@ import Foundation
 
 struct User: Codable {
     
-    
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    var birthdate = Date()
+    var extraNapkins = false
+    var frequentRefills = false
     
 }
