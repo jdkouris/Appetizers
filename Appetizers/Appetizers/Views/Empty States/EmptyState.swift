f50130c1d8b69035f340607b77f17321cea0e774
John Kouris <jkouris@nexient.com>
2021-03-18 07:56:35 -0700

    Added empty state file and image to assets

---
+++ Appetizers/Appetizers/Views/Empty States/EmptyState.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by John Kouris on 3/18/21.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    var body: some View {
+        
+        
+    }
+}
+
+struct EmptyState_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyState()
+    }
+}
