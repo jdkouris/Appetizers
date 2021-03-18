f23a7b31bee27c19cd7be56bd0d3853afc853897
John Kouris <jkouris@nexient.com>
2021-03-18 09:33:58 -0700

    Implemented orders, adding appetizers to order screen, and updating price label

--- Appetizers/Appetizers/Model/Order.swift
+++ Appetizers/Appetizers/Model/Order.swift
@@ -5,4 +5,22 @@
 //  Created by John Kouris on 3/18/21.
 //
 
-import Foundation
+import SwiftUI
+
+final class Order: ObservableObject {
+    
+    @Published var items = [Appetizer]()
+    
+    var totalPrice: Double {
+        items.reduce(0) { $0 + $1.price }
+    }
+    
+    func add(_ appetizer: Appetizer) {
+        items.append(appetizer)
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
+    
+}
--- Appetizers/Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Appetizers/Screens/OrderView/OrderView.swift
@@ -9,39 +9,35 @@ import SwiftUI
 
 struct OrderView: View {
     
-    @State private var orderItems = MockData.orderItems
+    @EnvironmentObject var order: Order
     
     var body: some View {
         NavigationView {
             ZStack {
                 VStack {
                     List {
-                        ForEach(orderItems) { appetizer in
+                        ForEach(order.items) { appetizer in
                             AppetizerListCell(appetizer: appetizer)
                         }
-                        .onDelete(perform: deleteItems)
+                        .onDelete(perform: order.deleteItems)
                     }
                     .listStyle(PlainListStyle())
                     
                     Button {
                         print("order placed")
                     } label: {
-                        APButton(title: "$99.99 - Place Order")
+                        APButton(title: "$\(order.totalPrice, specifier: "%.2f") - Place Order")
                     }
                     .padding(.bottom, 30)
                 }
                 
-                if orderItems.isEmpty {
+                if order.items.isEmpty {
                     EmptyState(imageName: "empty-order", message: "You have no items in your order.\n Please add an appetizer.")
                 }
                 
             }
             .navigationTitle("🧾 Orders")
         }
-    }
-    
-    func deleteItems(at offsets: IndexSet) {
-        orderItems.remove(atOffsets: offsets)
     }
 }
 
